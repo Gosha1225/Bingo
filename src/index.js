1ed5e8ab4150f5c1086829225506c7f1bab08ce6
Gosha1225 <1225gosh@gmail.com>
2022-07-14 16:50:09 +0400

    ironSrc

--- src/index.js
+++ src/index.js
@@ -48,6 +48,14 @@ function getMobileOperatingSystem() {
   }
 }
 
+function handleDownloadBtn(){
+  // window.open('https://apps.apple.com/us/app/empire-bingo/id1584334464')
+  // dapi.openStoreUrl();
+  console.log(dapi);
+}
+
+document.querySelector('.link').addEventListener('pointerdown', handleDownloadBtn)
+
 handleResize();
 downloadBtnAnimation();
 lightAnimation();
@@ -228,3 +236,49 @@ function doBingo() {
 }
 
 doBingo();
+
+window.onload = function(){
+  (dapi.isReady()) ? onReadyCallback() : dapi.addEventListener("ready", onReadyCallback); 
+  //here you can put other code that not related to dapi logic
+ };
+ 
+ function onReadyCallback(){
+  //no need to listen to this event anymore
+  dapi.removeEventListener("ready", onReadyCallback);
+  let isAudioEnabled = !!dapi.getAudioVolume();
+ 
+ 
+  if(dapi.isViewable()){
+   adVisibleCallback({isViewable: true});
+  }
+ 
+  dapi.addEventListener("viewableChange", adVisibleCallback);
+  dapi.addEventListener("adResized", adResizeCallback);
+  dapi.addEventListener("audioVolumeChange", audioVolumeChangeCallback);
+
+ }
+ 
+ function adVisibleCallback(event){
+  console.log("isViewable " + event.isViewable);
+  if (event.isViewable){
+   screenSize = dapi.getScreenSize();
+   //START or RESUME the ad
+  } else {
+   //PAUSE the ad and MUTE sounds
+  }
+ }
+ 
+ function adResizeCallback(event){
+  screenSize = event;
+  console.log("ad was resized width " + event.width + " height " + event.height);
+ }
+ 
+ function audioVolumeChangeCallback(volume){
+  let isAudioEnabled = !!volume;
+ 
+  if (isAudioEnabled){
+   //START or turn on the sound
+  } else {
+   //PAUSE the turn off the sound
+  }
+ }
